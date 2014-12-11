5fab09dc470d4b176c73156a97aa07c9c638b303
keanseng04 <keanseng04@gmail.com>
2014-12-11 20:01:16 +0800

    commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,30 @@
+require 'twitter'
+
+get '/' do
+  # Look in app/views/index.erb
+  erb :index
+end
+
+
+get '/:username' do
+  @username = params[:username]
+  @user = TwitterUser.find_or_create_by_username(params[:username])
+
+  if @user.tweets_stale?
+    puts "stale"
+    @new_user = $client.user_timeline(@user.username).first(10)
+    @new_user.each do |key|
+      @tweets = Tweet.find_or_create_by_content(content: key.text, twitter_user_id: @user.id, created_tweet: key.created_at)
+    end
+    tweet = @user.tweets.last
+    tweet.updated_at= Time.now
+    tweet.save
+  end
+
+  erb :username
+end
+
+post '/username' do
+
+  redirect "/#{params[:username]}"
+end
